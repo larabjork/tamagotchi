645bac8210d8ff3866d336fd4ad3d24128f97e4c
Student <example@example.com>
2019-11-18 11:53:02 -0800

    add test 2

--- __tests__/tamagotchi-test.js
+++ __tests__/tamagotchi-test.js
@@ -9,9 +9,17 @@ describe('Katya', () => {
 
   test('should have a name and levels for feed, play, and sleep when it is created', () => {
     expect(pet.name).toEqual("Katya");
-    expect(pet.feedLevel).toEqual(10);
-    expect(pet.playLevel).toEqual(10);
-    expect(pet.sleepLevel).toEqual(10);
+    expect(pet.feedLevel).toEqual(5);
+    expect(pet.playLevel).toEqual(5);
+    expect(pet.sleepLevel).toEqual(5);
+  });
+
+  test('when play is clicked, +1 to play, -1 to feed and sleep', () => {
+    pet.clickPlay();
+    expect(pet.feedLevel).toEqual(4);
+    expect(pet.playLevel).toEqual(6);
+    expect(pet.sleepLevel).toEqual(4);
+
   });
 
 
--- src/tamagotchi.js
+++ src/tamagotchi.js
@@ -1,1 +1,11 @@
-// export function
+export class Pet {
+  constructor(name) {
+    this.name = name;
+    this.feedLevel = 5;
+    this.playLevel = 5;
+    this.sleepLevel = 5;
+  }
+
+  
+
+}
