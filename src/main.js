94d890b1065a97beb21260b7db5d54174a530876
Student <example@example.com>
2019-11-19 13:49:16 -0800

    add additional styling, add game over page and refresh page

--- src/main.js
+++ src/main.js
@@ -6,12 +6,14 @@ import './styles.css';
 
 $(document).ready(function() {
   const newPet = new Pet ();
+  $("#x").delay(500).fadeIn(200);
+  $("#treme").delay(750).slideDown(500).fadeIn(200);
   $('#start-form').submit(function(event) {
     event.preventDefault();
     const inputtedName = $('#choose-name').val();
     $('#choose-name').val("");
 
-    $("#pet-name").html(inputtedName);
+    $(".pet-name").html(inputtedName);
 
     $('#game-play').show();
     $('#name-display').show();
@@ -22,6 +24,14 @@ $(document).ready(function() {
       $('#fun-counter').html(newPet.playLevel);
       $('#energy-counter').html(newPet.sleepLevel);
     }, 1000);
+
+    setInterval( () => {
+      if (newPet.gameStatus() === false) {
+        $("#end").show();
+        $("#game-play").hide();
+      }
+    }, 1000);
+
 
     $("#hunger-button").click(function(){
       newPet.clickFeed();
@@ -38,6 +48,10 @@ $(document).ready(function() {
     newPet.playTimer();
     newPet.sleepTimer();
     newPet.feedTimer();
+
+    $("#restart-button").click(function(){
+      location.reload();
+    });
 
   });
 });
